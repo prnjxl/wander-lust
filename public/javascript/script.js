fef4036fcd7d36813489f4a7063f20b8cb06aa15
Pranjal Piyush <pranjalpiyush29@gmail.com>
2025-07-11 20:56:53 +0530

    changes to repo

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -30,3 +30,16 @@
     rangeOutput.textContent = this.value;
   });
 }
+
+//tax switch functionality
+let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+        taxSwitch.addEventListener("click", () => {
+            let taxInfo = document.getElementsByClassName("tax-info");
+            for(info of taxInfo){
+                if(info.style.display != "inline"){
+                    info.style.display = "inline";
+                } else {
+                    info.style.display = "none";
+                }
+            }
+        });
